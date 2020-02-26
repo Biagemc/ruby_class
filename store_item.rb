5ba89547a59eafea0e6e0d08e1ba02bd468ea148
biagemc <biagemc@gmail.com>
2020-02-25 20:46:57 -0500

    class reader and writer update

--- store_item.rb
+++ store_item.rb
@@ -1,33 +1,11 @@
-# Rewrite your store items using a class instead of a hash.
-# a) Choose which attributes should have “reader” methods and which attributes should have “writer” methods.
-# b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
-
 class Store
-  def initialize(category_input, product_input, color_input, price_input)
-    @category = category_input
-    @type = type_input
+  attr_reader :type, :color, :price
+  attr_writer :color, :price
+
+  def initialize(product_input, color_input, price_input)
+    @type = product_input
     @color = color_input
     @price = price_input
-  end
-
-  def type
-    @type
-  end
-
-  def color
-    @color
-  end
-
-  def price
-    @price
-  end
-
-  def color=(product_color)
-    @color = product_color
-  end
-
-  def price=(product_price)
-    @price = price
   end
 end
 
