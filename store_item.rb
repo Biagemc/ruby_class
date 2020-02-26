3255eaad0defc45c9c3950230a637817d0f3589c
biagemc <biagemc@gmail.com>
2020-02-25 20:40:14 -0500

    class structure exercise

--- store_item.rb
+++ store_item.rb
@@ -1,19 +1,44 @@
-# A. Use the terminal to create a new folder in your actualize folder called object_oriented_ruby.
-# B. Create a new file in the folder called store_item.rb and open it in your text editor.
-# C. Use hashes with symbols to represent the following scenario:
-# C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+# Rewrite your store items using a class instead of a hash.
+# a) Choose which attributes should have “reader” methods and which attributes should have “writer” methods.
+# b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
 
-fruits = { type: "banana", color: "yellow", price: 0.79 }
+class Store
+  def initialize(category_input, product_input, color_input, price_input)
+    @category = category_input
+    @type = type_input
+    @color = color_input
+    @price = price_input
+  end
 
-apple = { type: "apple", color: "red", price: 4.59 }
+  def type
+    @type
+  end
 
-peach = { type: "peach", color: "green", price: 3.20 }
+  def color
+    @color
+  end
 
-# C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
-# C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
-# Bonus: Read more about Ruby’s hash symbol syntax:
-# - http://alwayscoding.ca/momentos/2012/06/15/ruby-hash-syntax-hashrocket-vs-json-style/
-# - Create a hash in irb where both the keys and values are symbols. Which syntax do you prefer?
-# { always: 'coding' }
-# Ruby Hash Syntax: Hashrocket vs JSON-Style
-# At a first glance the 1.9 ruby hash syntax looks like a simple change. No longer using the infamous “Hashrocket” and moving to a familiar JavaScript Object Notation. {:cat => 'meow'} # 1.8 {cat: 'meow'} # 1.9 On an average day I write equally as much JavaScript as I do Ruby so the new 1.9 syntax is an easy swtich for me. Do these updates to the syntax mean that we’ll be seeing the demise of the 1.
+  def price
+    @price
+  end
+
+  def color=(product_color)
+    @color = product_color
+  end
+
+  def price=(product_price)
+    @price = price
+  end
+end
+
+banana = Store.new("banana", "yellow", 0.79)
+
+p banana.price
+p banana.color
+p banana.type
+
+banana.color = "green"
+
+p "this is a new color of banana:"
+p banana.color
+p "___________"
